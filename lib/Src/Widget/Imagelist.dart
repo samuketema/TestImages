ef0024dc55911937870493d38eb3e82245bee486
Samuel <samuelketema01@gmail.com>
2024-03-15 22:27:27 +0300

    padding property and borders added

--- lib/Src/Widget/Imagelist.dart
+++ lib/Src/Widget/Imagelist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import '../models/Image_model.dart';
 
 class ImagesList extends StatelessWidget {
@@ -10,6 +11,10 @@ class ImagesList extends StatelessWidget {
       itemCount: images.length,
       itemBuilder: (context, int index) {
         return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
+          padding:EdgeInsets.all(20.0),
           margin:EdgeInsets.all(20.0) ,
           child:Image.network(images[index].url)
         )

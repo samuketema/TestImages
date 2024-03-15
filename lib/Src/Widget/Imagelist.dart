f98be2d198096dff7c09afa965399e4e1f67bd3d
Samuel <samuelketema01@gmail.com>
2024-03-15 15:49:41 +0300

    added the Image.network

---
+++ lib/Src/Widget/Imagelist.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../models/Image_model.dart';
+
+class ImagesList extends StatelessWidget {
+  final List<ImageModel> images;
+  ImagesList(this.images);
+  @override
+  Widget build(BuildContext context) {
+    return  ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index) {
+        return Image.network(images[index].url);
+      },
+      );
+  }
+}

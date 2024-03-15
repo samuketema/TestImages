c2960269b77fd6681f5f0159804e33097a4a7acd
Samuel <samuelketema01@gmail.com>
2024-03-15 15:53:09 +0300

    Error fixed by initializing null instance variables of the ImageModel class

--- lib/Src/models/Image_model.dart
+++ lib/Src/models/Image_model.dart
@@ -1,11 +1,11 @@
-Class ImageModel{
-  int id ;
-  String title;
-  String url;
+class ImageModel{
+  int id  = 0;
+  String title = '';
+  String url= '';
 
-  Imagemodel(this.id , this.url , this.title)
+  ImageModel(this.id , this.url , this.title);
 
-  ImageModel.fromJson(parsedJson){
+  ImageModel.fromJson(Map<String, dynamic> parsedJson){
     id = parsedJson['id'];
     url = parsedJson['url'];
     title = parsedJson['title'];

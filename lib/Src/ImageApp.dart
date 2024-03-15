79bc1269bc39f23e636197dab63b5319834a6d0f
Samuel <samuelketema01@gmail.com>
2024-03-15 14:20:50 +0300

    make it to display the image list instead of text message

--- lib/Src/ImageApp.dart
+++ lib/Src/ImageApp.dart
@@ -1,29 +1,39 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'models/Image_model.dart';
+import 'package:http/http.dart';
+import 'Widget/Imagelist.dart'
 
 class App extends StatefulWidget{
 
   createState(){
-    return AppState();
+    return AppState(); 
   }
  
 }
 
 class AppState extends State<App>{
   int counter = 0;
+  List<ImageModel> images = [];
+  void fetchImage () async{
+    counter++;
+   var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+   var imageModel = ImageModel.fromJson(jsonDecode(response));
+   setState((){
+   images.add.(imageModel);
+  });
+  }
+
 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('$counter'),
+        body: ImageList(),
         appBar: AppBar(
           title: const Text('Gallery Of Objects'),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed:(){
-            setState((){
-              counter++;
-            });
-          },
+          onPressed:fetchData,
           child:Icon(Icons.add),
         )
         ),
